dd2d1a6355dd62105d55074e8d23ea6a2cedc36d
Aly Kafoury <aly.kafoury@gmail.com>
2018-12-30 23:37:34 +0200

    Jenkinsfile

--- provision-cluster.sh
+++ provision-cluster.sh
@@ -1,3 +1,3 @@
 ./scripts/post-kube.sh clean=false
-# ./helm.sh init --service-account tiller
-# ./helm.sh install --name jenkins --namespace default stable/jenkins -f helm/jenkins/values.yml
+./helm.sh init --service-account tiller
+./helm.sh install --name jenkins --namespace default stable/jenkins -f helm/jenkins/values.yml
