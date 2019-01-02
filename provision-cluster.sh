04e7c4174b58eebca8f09c352f0279a730784d2f
Aly Kafoury <aly.kafoury@gmail.com>
2019-01-02 20:03:40 +0200

    finish automating provision cluster

--- provision-cluster.sh
+++ provision-cluster.sh
@@ -1,1 +1,5 @@
-./scripts/post-kube.sh clean=false
+./scripts/post-kube.sh clean=false;
+ls manifests | xargs -I {} ./kubectl.sh apply -f manifests/{};
+cd notejam;
+docker build . -t docker-registry:31000/notejam:${gitCommit};
+docker push docker-registry:31000/notejam:${gitCommit};
