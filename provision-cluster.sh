fd7c37246193532c7e4568ddc0346f86bc6e999b
Aly Kafoury <aly.kafoury@gmail.com>
2019-01-02 23:47:37 +0200

    Read me

--- provision-cluster.sh
+++ provision-cluster.sh
@@ -2,5 +2,5 @@
 ./scripts/post-kube.sh clean=false;
 ls manifests | xargs -I {} ./kubectl.sh apply -f manifests/{};
 cd notejam;
-docker build . -t docker-registry:31000/notejam:${gitCommit};
-docker push docker-registry:31000/notejam:${gitCommit};
+docker build . -t docker-registry:31000/notejam-app:latest;
+docker push docker-registry:31000/notejam-app:latest;
